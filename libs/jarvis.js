7085837b11309c6de34f0b04edd61d317a2b0acc
Arjun <86394506+Arjun-M@users.noreply.github.com>
2021-10-16 22:03:34 +0530

    Update jarvis.js

--- libs/jarvis.js
+++ libs/jarvis.js
@@ -1,6 +1,8 @@
 let libPrefix = "jarvis"
 
 function onMasterCommand() {
+  var anti_forward = Bot.getProperty("ANTI-FORWARD" + chat.chatid)
+  var anti_via = Bot.getProperty ("ANTI-VIABOT"+chat.chatid)
   if (request) {
     if (request.new_chat_members[0].username == bot.name) {
       Bot.sendInlineKeyboard(
@@ -8,6 +10,20 @@ function onMasterCommand() {
         "Thanks For Adding Me To Your Group .Don't Forget To Make Me As Admin Of The Group Or I Will Not Be Able To Manage This Group \n\n   *Thank You 😇*"
       )
       return
+    } else if (request.forward_from || request.forward_from_chat) {
+      if (anti_forward == "ON") {
+        Api.deleteMessage({
+          chat_id: chat.chatid,
+          message_id: request.message_id
+        })
+      } 
+    } else if (request.via_bot){
+    if (anti_via == "ON") {
+        Api.deleteMessage({
+          chat_id: chat.chatid,
+          message_id: request.message_id
+        })
+      } 
     }
   }
 }
