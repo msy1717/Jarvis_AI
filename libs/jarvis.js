5bc1df7beee98ec5ddba8ee22a27b41dd69b9d6e
Arjun <86394506+Arjun-M@users.noreply.github.com>
2021-10-16 22:10:59 +0530

    Update jarvis.js

--- libs/jarvis.js
+++ libs/jarvis.js
@@ -2,7 +2,8 @@ let libPrefix = "jarvis"
 
 function onMasterCommand() {
   var anti_forward = Bot.getProperty("ANTI-FORWARD" + chat.chatid)
-  var anti_via = Bot.getProperty ("ANTI-VIABOT"+chat.chatid)
+  var anti_via = Bot.getProperty("ANTI-VIABOT" + chat.chatid)
+  var anti_bot = Bot.getProperty("ANTI-BOT" + chat.chatid)
   if (request) {
     if (request.new_chat_members[0].username == bot.name) {
       Bot.sendInlineKeyboard(
@@ -16,16 +17,23 @@ function onMasterCommand() {
           chat_id: chat.chatid,
           message_id: request.message_id
         })
-      } 
-    } else if (request.via_bot){
-    if (anti_via == "ON") {
+      }
+    } else if (request.via_bot) {
+      if (anti_via == "ON") {
         Api.deleteMessage({
           chat_id: chat.chatid,
           message_id: request.message_id
         })
-      } 
+      }
+    } else if (request.new_chat_members[0].is_bot == true) {
+      if (anti_bot == "ON") {
+        Api.kickChatMember({
+          chat_id: chat.chatid,
+          user_id: request.message.from.id
+        })
+      }
     }
-  }
-}
+  } // request closing
+} // function closing
 
 on("*", onMasterCommand)
