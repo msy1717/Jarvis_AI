beaedd32a8765aca7d8f42a74af4891aaaad517e
Arjun <86394506+Arjun-M@users.noreply.github.com>
2021-10-17 16:03:19 +0530

    Update jarvis.js

--- libs/jarvis.js
+++ libs/jarvis.js
@@ -4,15 +4,24 @@ function onMasterCommand() {
   var anti_forward = Bot.getProperty("ANTI-FORWARD" + chat.chatid)
   var anti_via = Bot.getProperty("ANTI-VIABOT" + chat.chatid)
   var anti_bot = Bot.getProperty("ANTI-BOT" + chat.chatid)
+  var newUser = Bot.getProperty("WelcomeText" + chat.chatid)
+  var leftUser = Bot.getProperty("LeftText" + chat.chatid)
   if (request) {
-    if(new_members.length > 0){
-    if (request.new_chat_members[0].username == bot.name) {
-      Bot.sendInlineKeyboard(
-        [[{ title: "🎥 Tutorial", command: "/tutorial" }]],
-        "Thanks For Adding Me To Your Group .Don't Forget To Make Me As Admin Of The Group Or I Will Not Be Able To Manage This Group \n\n   *Thank You 😇*"
-      )
-      return
-    }
+    if (request.new_chat_members.length > 0) {
+      if (request.new_chat_members[0].username == bot.name) {
+        Bot.sendInlineKeyboard(
+          [[{ title: "🎥 Tutorial", command: "/tutorial" }]],
+          "Thanks For Adding Me To Your Group .Don't Forget To Make Me As Admin Of The Group Or I Will Not Be Able To Manage This Group \n\n   *Thank You 😇*"
+        )
+        return
+      } else if (request.new_chat_members.length > 0) {
+        var WelcomeMsg = newUser.replace("{name}",request.new_chat_members[0].first_name)
+        WelcomeMsg = WelcomeMsg.replace("{id}", request.new_chat_members[0].id)
+        WelcomeMsg = WelcomeMsg.replace("{username}", request.new_chat_members[0].username)
+        Bot.sendMessage(WelcomeMsg)
+      }
+    } else if (request.left_chat_member) {
+      Bot.sendMessage(leftUser)
     } else if (request.forward_from || request.forward_from_chat) {
       if (anti_forward == "ON") {
         Api.deleteMessage({
