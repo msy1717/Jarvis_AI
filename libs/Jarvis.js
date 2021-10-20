c9cd7a920733dc3f781f52377f440925692f000a
Arjun <86394506+Arjun-M@users.noreply.github.com>
2021-10-20 10:28:11 +0530

    Update Jarvis.js

--- libs/Jarvis.js
+++ libs/Jarvis.js
@@ -13,10 +13,7 @@ if (chat.chat_type !== "private") {
     }
   } else if (request.new_chat_members.length > 0) {
     if (request.new_chat_members[0].username == bot.name) {
-      Bot.sendInlineKeyboard(
-        [[{ title: "🎥 Tutorial", command: "/tutorial" }]],
-        "Thanks For Adding Me To Your Group .Don't Forget To Make Me As Admin Of The Group Or I Will Not Be Able To Manage This Group \n\n   *Thank You 😇*"
-      )
+      Bot.sendInlineKeyboard([[{ title: "🎥 Tutorial", command: "/tutorial" }]],"Thanks For Adding Me To Your Group .Don't Forget To Make Me As Admin Of The Group Or I Will Not Be Able To Manage This Group \n\n   *Thank You 😇*" )
       return
     } else if (request.new_chat_members.length > 0) {
       var WelcomeMsg = newUser.replace("{name}",request.new_chat_members[0].first_name )
