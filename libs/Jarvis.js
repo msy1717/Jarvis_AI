b0f0cd34ef38aa51a3f8e60bb1f6750a4e0039e6
Arjun <86394506+Arjun-M@users.noreply.github.com>
2021-10-23 08:03:06 +0530

    Update Jarvis.js

--- libs/Jarvis.js
+++ libs/Jarvis.js
@@ -1,17 +1,16 @@
-let LibPrefix = "JARVIS" 
+let LibPrefix = "JARVIS"
 
-function paste(text){
-HTTP.post({
-      url: "https://nekobin.com/api/documents",
-      success: "/onPaste",
-      body: { content: request.reply_to_message.text }
-    })
-
+function paste(text) {
+  HTTP.post({
+    url: "https://nekobin.com/api/documents",
+    success: "/onPaste",
+    body: { content: request.reply_to_message.text }
+  })
 }
-function onPaste (){
-var json = JSON.parse (content)
-var link = "https://nekobin.com/"+json.result.key
-return link
+function onPaste() {
+  var json = JSON.parse(content)
+  var link = "https://nekobin.com/" + json.result.key
+  return link
 }
 
-on(libPrefix + 'onPaste', onPaste )
+on(libPrefix + "onPaste", onPaste)
