fcb6f1e605bfc4293266ead170493dd5af5a920e
Arjun <86394506+Arjun-M@users.noreply.github.com>
2021-10-23 08:17:11 +0530

    Update Jarvis.js

--- libs/Jarvis.js
+++ libs/Jarvis.js
@@ -4,13 +4,16 @@ function paste(text) {
   HTTP.post({
     url: "https://nekobin.com/api/documents",
     success: "/onPaste",
-    body: { content: request.reply_to_message.text }
+    body: { content: text }
   })
 }
 function onPaste() {
   var json = JSON.parse(content)
   var link = "https://nekobin.com/" + json.result.key
   return link
+}
+function chat_bot(){
+
 }
 
 on(libPrefix + "onPaste", onPaste)
